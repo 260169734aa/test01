6ba9b0f029cc178648201336d8a3fa318c35d955
26016 <26016@DESKTOP-G88AKOS>
2019-04-25 19:53:45 +0800

    this is lyz project

--- src/a/a.java
+++ src/a/a.java
@@ -5,11 +5,12 @@
  * @author 26016
  *
  */
-package a;
+
  public class a{
 	 	 public static void main(String[] args) {
 			 System.out.println("hello git");
-			 	
-		}
+			 	 System.out.println("");
+			 	  System.out.println("我丢这样就好用了哦");
+	 	 }
 	 
  }
